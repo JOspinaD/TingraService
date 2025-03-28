32a7e4987932475e99e71bf122d877ca11a39c7e
johan <johan@JOHANDELACRUZ>
2025-03-28 10:53:24 -0500

    feat: add Models Empresa, preguntas

---
+++ TingraService/Models/preguntas.cs
@@ -0,0 +1,11 @@
+﻿using TingraService.Models.Base;
+
+namespace TingraService.Models
+{
+    public class Pregunta : BaseEntity
+    {
+        public string Preguntas { get; set; }
+        public string? Descripcion {  get; set; }
+        public Guid IdEmpresa { get; set; }
+    }
+}
