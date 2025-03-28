84628e96e51e8f18fce25af6fe9f70c956d01889
johan <johan@JOHANDELACRUZ>
2025-03-28 10:57:34 -0500

    feat: add Services Empresa, Pregunta

---
+++ TingraService/BLL/Services/Contract/IEmpresasService.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+using TingraService.Common;
+using TingraService.DTO.Empresa;
+using TingraService.Models;
+
+namespace TingraService.BLL.Services.Contract
+{
+    public interface IEmpresaService
+    {
+        Task<Result<IEnumerable<EmpresaReadDto>>> GetAllAsync();
+        Task<Result<EmpresaReadDto>> GetByIdAsync(Guid id);
+        Task<int> CountAsync(Expression<Func<Empresa, bool>> filter);
+        Task<Result<EmpresaReadDto>> CreateAsync(EmpresaWriteDto empresaWriteDto);
+        Task<Result<EmpresaReadDto>> UpdateAsync(Guid id, EmpresaWriteDto empresaWriteDto);
+        Task<Result> DeleteAsync(Guid id);
+    }
+}
