4707468de747cffbb9d927854e03782c86b8f415
johan <johan@JOHANDELACRUZ>
2025-03-28 10:54:33 -0500

    feat: add DTOs for Empresa, Pregunta

---
+++ TingraService/DTO/Pregunta/PreguntaswriteDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TingraService.DTO.Pregunta
+{
+    public class PreguntaWriteDto
+    {
+        public string Texto { get; set; }
+        public string Descripcion { get; set; }
+        public Guid? IdEmpresa { get; set; }
+    }
+}
