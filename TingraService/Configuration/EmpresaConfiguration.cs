e146ab4fed685380140f56b327ce680eb2500694
juan <juanpabloospina19@gmail.com>
2025-04-23 16:18:13 -0500

    feat: Add configuration for Empresa and Pregunta

---
+++ TingraService/Configuration/EmpresaConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TingraService.Models;
+
+namespace TingraService.Configuration
+{
+    public class EmpresaConfiguration : IEntityTypeConfiguration<Empresa>
+    {
+        public void Configure(EntityTypeBuilder<Empresa> builder)
+        {
+            builder.ToTable(nameof(Empresa));
+
+            builder.Property(fd => fd.CreatedAt)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            builder.Property(fd => fd.UpdatedAt)
+                .IsRequired()
+                .ValueGeneratedOnUpdate();
+        }
+    }
+}
