bd5e47855c6e05089d98f3d083d17d619b748e86
juan <juanpabloospina19@gmail.com>
2025-06-02 18:33:15 -0500

    feat: Add new fields for Empresa

--- TingraService/DTO/Empresa/EmpresasWriteDto.cs
+++ TingraService/DTO/Empresa/EmpresasWriteDto.cs
@@ -3,8 +3,20 @@
     public class EmpresaWriteDto
     {
         public string Nombre { get; set; }
-        public string Dirreccion { get; set; }
+        public string Direccion { get; set; }
         public string? Telefono { get; set; }
         public string? Email { get; set; }
+        public string? Propietario { get; set; }
+        public DateOnly? FechaCreacion { get; set; }
+        public string? Servicios { get; set; }
+        public string? RedesSociales { get; set; }
+        public bool PerteneceRedEmprendedores { get; set; }
+        public bool RedEmprendedoresConfirmada { get; set; }
+        public string? AspectosMejorar { get; set; }
+        public string? Capacitadores { get; set; }
+        public DateTime? FechaLlamada { get; set; }
+        public string? Observaciones { get; set; }
+        public string? Disponibilidad { get; set; }
+        public bool CapacitacionRecibida { get; set; }
     }
 }
