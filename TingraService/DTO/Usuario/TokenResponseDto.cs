0c49272c51ddbc424714b6e6f70e1264a48111a4
juan <juanpabloospina19@gmail.com>
2025-06-10 23:07:23 -0500

    feat: Add new DTO for response Token

---
+++ TingraService/DTO/Usuario/TokenResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TingraService.DTO.Usuario
+{
+    public class TokenResponseDto
+    {
+        public required string AccessToken { get; set; }
+        public required string RefreshToken { get; set; }
+    }
+}
