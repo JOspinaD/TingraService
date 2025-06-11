de87c45d0f8be8272ff74abb8a8a35ba3e93dfe0
juan <juanpabloospina19@gmail.com>
2025-06-10 23:06:58 -0500

    fix: Change logic for login and refreshToken

--- TingraService/BLL/Services/UsuarioServices/UsuarioService.cs
+++ TingraService/BLL/Services/UsuarioServices/UsuarioService.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Azure.Core;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -42,34 +43,38 @@ namespace TingraService.BLL.Services.UsuarioServices
 
         }
 
-        public async Task<Result<LoginResponseDto>> LoginAsync(LoginDto loginDto)
+        public async Task<Result<TokenResponseDto>> LoginAsync(UsuarioWriteDto usuarioWriteDto)
         {
             try
             {
-                var usuario = await _repository.GetBy(t => t.Correo == loginDto.Correo);
+                var usuario = await _repository.GetBy(t => t.Correo == usuarioWriteDto.Correo);
 
                 if (usuario == null)
-                    return Result.Failure<LoginResponseDto>(UsuarioErrors.NotExists);
+                    return Result.Failure<TokenResponseDto>(UsuarioErrors.NotExists);
 
                 var isValid = _passwordService.verifyPassword(
-                    loginDto.Contraseña,
+                    usuarioWriteDto.HashContraseña,
                     usuario.HashContraseña,
                     usuario.Salt
                 );
 
                 if (!isValid)
-                    return Result.Failure<LoginResponseDto>(UsuarioErrors.InvalidPassword);
-               
-                var response = _mapper.Map<LoginResponseDto>(usuario);
-                response.Token = CreateToken(usuario);
-               
+                    return Result.Failure<TokenResponseDto>(UsuarioErrors.InvalidPassword);
+
+                var response = new TokenResponseDto
+                {
+                    AccessToken = CreateToken(usuario),
+                    RefreshToken = await GenerateAndSaveRefreshTokenAsync(usuario)
+                };
+
+
 
                 return Result.Success(response);
             }
             catch (Exception ex)
             {
                 Console.Out.WriteLine("--> ERROR: ", ex.Message);
-                return Result.Failure<LoginResponseDto>(UsuarioErrors.Unhandled);
+                return Result.Failure<TokenResponseDto>(UsuarioErrors.Unhandled);
             }
         }
 
