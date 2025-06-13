fdd4d4ccfc6ea73d7fa6e6778c82c836a1f52b5b
juan <juanpabloospina19@gmail.com>
2025-06-13 17:28:35 -0500

    feat: Add Dto for RefreshToken

---
+++ TingraService/DTO/Usuario/RefreshTokenRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TingraService.DTO.Usuario
+{
+    public class RefreshTokenRequestDto
+    {
+        public Guid UsuarioId { get; set; }
+        public required string RefreshToken { get; set; }
+
+    }
+}
